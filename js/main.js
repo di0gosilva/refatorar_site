aa91f265c2a816d1aadcc1290b3f99ccd6b0b2dd
Diogo <diogo_silva8@estudante.sesisenai.org.br>
2025-08-18 15:36:33 -0300

    cards de produtos finalizado

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-// main.js
-import Produto from './Produto.js';
-import Cliente from './Cliente.js';
+const produtos = [
+{ img: "./imagens/img0.jpg", nome: "Produto 1", preco: 50 },
+{ img: "./imagens/img1.jpg", nome: "Produto 2", preco: 60 },
+{ img: "./imagens/img2.jpg", nome: "Produto 3", preco: 75 },
+{ img: "./imagens/img3.jpg", nome: "Produto 4", preco: 120 },
+{ img: "./imagens/img4.jpg", nome: "Produto 5", preco: 30 },
+{ img: "./imagens/img5.jpg", nome: "Produto 6", preco: 60 },
+{ img: "./imagens/img6.jpg", nome: "Produto 7", preco: 80 },
+{ img: "./imagens/img7.jpg", nome: "Produto 8", preco: 100 },
+{ img: "./imagens/img8.jpg", nome: "Produto 9", preco: 45 },
+{ img: "./imagens/img9.jpg", nome: "Produto 10", preco: 90 },
+{ img: "./imagens/img10.jpg", nome: "Produto 11", preco: 50 },
+{ img: "./imagens/img11.jpg", nome: "Produto 12", preco: 60 },
+{ img: "./imagens/img12.jpg", nome: "Produto 13", preco: 70 },
+{ img: "./imagens/img13.jpg", nome: "Produto 14", preco: 80 },
+];
 
-let produtos = JSON.parse(localStorage.getItem('produtos') || '[]')
-    .map(Produto.fromJSON);
+const container = document.getElementById("listaProdutos");
 
-let clientes = JSON.parse(localStorage.getItem('clientes') || '[]')
-    .map(Cliente.fromJSON);
+produtos.forEach(prod => {
+const card = document.createElement("div");
+card.classList.add("card");
 
-function cadastrarProduto() {
-    const nome = document.getElementById('produto').value;
-    const descricao = document.getElementById('descricao').value;
-    const codigo = document.getElementById('codigo').value;
-    const preco = document.getElementById('preco').value;
-    const link = document.getElementById('linkAmazon').value;
+card.innerHTML = `
+    <img src="${prod.img}" alt="${prod.nome}">
+    <h3>${prod.nome}</h3>
+    <p>R$ ${prod.preco.toFixed(2)}</p>
+`;
 
-    const novoProduto = new Produto(nome, descricao, codigo, preco, link);
-
-    produtos.push(novoProduto);
-    localStorage.setItem('produtos', JSON.stringify(produtos.map(p => p.toJSON())));
-
-    alert('Produto cadastrado com sucesso!');
-}
+container.appendChild(card);
+});
